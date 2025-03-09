c7b14b7c9e5716cc20c4fcf27ce044ade45a6205
Farisahilmi <farisahilmiexe@gmail.com>
2025-03-10 04:34:39 +0700

    Update Tugas_2.py
    perubahan ketiga

--- Tugas_2.py
+++ Tugas_2.py
@@ -1,1 +1,8 @@
+angka = int(input("masukan nilai yang ingin diperiksa : "))
 
+if angka > 0 :
+    print("angka nya positif :", angka)
+elif angka < 0:
+    print("angka nya negatif :", angka)
+else:
+    print("angka nya sama dengan nol : ", angka)
