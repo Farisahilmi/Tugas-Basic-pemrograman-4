9e47c2abee31ca63be07bfd6ada126ed9a05099a
Farisahilmi <farisahilmiexe@gmail.com>
2025-03-09 13:44:49 +0700

    Perubahan ketiga

--- Tugas_3.py
+++ Tugas_3.py
@@ -1,1 +1,14 @@
+jumlah_uang = int(input("Total uang: "))
+total_belanja = int(input("Total belanja: "))
 
+if total_belanja > 100000:
+    diskon = total_belanja * 0.10
+    print("Dapat diskon 10%:", int(diskon))
+elif total_belanja > 50000:
+    diskon = total_belanja * 0.05
+    print("Dapat diskon 5%:", int(diskon))
+else:
+    diskon = 0  
+    print("Tidak dapat diskon")
+total_setelah_diskon = total_belanja - diskon
+print("Total yang harus dibayar:", int(total_setelah_diskon))
