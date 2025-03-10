9312e5089f8ca6fadb76119ecdf6c9e207830c00
Farisahilmi <farisahilmiexe@gmail.com>
2025-03-10 08:55:35 +0700

    Update Tugas_1.py
    Prubahan

--- Tugas_1.py
+++ Tugas_1.py
@@ -1,6 +1,6 @@
 tahun = int(input("Masukkan tahun : "))
 
-if (tahun % 400 == 0) or (tahun % 4 == 0):
+if (tahun % 100 != 0) or (tahun % 4 == 0):
     print("Tahun kabisat")
 else:
     print("Bukan tahun kabisat")
